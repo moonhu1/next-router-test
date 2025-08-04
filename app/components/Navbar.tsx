f95bf3b119d4f3fdca1282c2be5993cf879e29f5
moonhu1 <skylarhuhu@gmail.com>
2025-08-04 09:59:33 +0800

    导航栏样式添加

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,11 +1,35 @@
 import Link from "next/link";
+
 export default function Navbar() {
   return (
-    <nav>
-      <Link href="/"> 11Home </Link>
-      <Link href="/courses"> 课程列表 </Link>
-      <Link href="/login"> 用户登录 </Link>
-      <Link href="/blog"> 博客 </Link>
+    <nav className="bg-white shadow py-4">
+      <div className="flex justify-center space-x-8">
+        <Link
+          href="/"
+          className="text-gray-700 hover:text-blue-500 transition-colors"
+        >
+          Home
+        </Link>
+        <Link
+          href="/login"
+          className="text-gray-700 hover:text-blue-500 transition-colors"
+        >
+          用户登录
+        </Link>
+        <Link
+          href="/courses"
+          className="text-gray-700 hover:text-blue-500 transition-colors"
+        >
+          课程列表
+        </Link>
+
+        <Link
+          href="/blog"
+          className="text-gray-700 hover:text-blue-500 transition-colors"
+        >
+          博客
+        </Link>
+      </div>
     </nav>
   );
 }
